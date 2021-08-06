5b4682a67cd6d6abb5424b913204e33873986b41
vincen-tho <16520229@std.stei.itb.ac.id>
2021-08-06 21:16:31 +0700

    refactor code

--- traze/src/App.js
+++ traze/src/App.js
@@ -1,32 +1,10 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
-import './App.css';
-import Dashboard from './components/Dashboard/Dashboard';
-import Preferences from './components/Preferences/Preferences';
-import Login from './components/Login/Login';
+import LoginApp from './components/LoginApp/LoginApp';
 
 function App() {
-  const [token, setToken] = useState();
-
-  if (!token) {
-    return <Login setToken={setToken} />;
-  }
-
   return (
-    <div className="wrapper">
-      <h1>Applications</h1>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/preferences">
-            <Preferences />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <>
+      <LoginApp />
+    </>
   );
 }
 
