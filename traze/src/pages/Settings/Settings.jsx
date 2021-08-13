519e4b723a6a5c8364912152d970249223857d83
vincen-tho <16520229@std.stei.itb.ac.id>
2021-08-14 02:58:19 +0700

    Added Setting page

--- traze/src/pages/Settings/Settings.jsx
+++ traze/src/pages/Settings/Settings.jsx
@@ -1,11 +1,19 @@
-import './Settings.css'
+import './Settings.css';
+import settings from '../../img/setting.png';
 
 const Settings = () => {
-  return ( 
-    <div>
-      
+  return (
+    <div className="settings">
+      <img src={settings} id="setting" />
+      <h2>Settings</h2> <br />
+      <div className="card">
+        <h4 className="text">Facebook & Twitter</h4> <br />
+        <h4 className="text">Save Photos</h4> <br />
+        <h4 className="text">Submit Feedback</h4> <br />
+        <h4 className="text">Clear History</h4>
+      </div>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Settings;
