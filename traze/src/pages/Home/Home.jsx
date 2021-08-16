5cc40d1c8d0d21cbe340f8fe8668f27e0edbf4f7
IloveNooodles <mgarebaldhie80@gmail.com>
2021-08-16 11:46:37 +0700

    styling map

--- traze/src/pages/Home/Home.jsx
+++ traze/src/pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import "./Home.css";
+import pinpointImg from "../../img/pinpoint.png";
 import Layout from "../../Layout";
 import Searchbar from "../../components/Searchbar/Searchbar";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import { useState } from "react";
+
+import * as dataSampah from "../../data/sampah.json";
 
 const Home = () => {
   const [viewport, setViewport] = useState({
@@ -12,6 +15,9 @@ const Home = () => {
     height: "100vh",
     zoom: 12,
   });
+
+  const [selectedLocation, setSelectedLocation] = useState(null);
+
   return (
     <Layout>
       <div className="Home">
@@ -19,8 +25,42 @@ const Home = () => {
         <ReactMapGL
           {...viewport}
           onViewportChange={(nextViewport) => setViewport(nextViewport)}
+          mapStyle="mapbox://styles/ilovenoodles/ckscxwr801t9417uqn500n3t2"
           mapboxApiAccessToken="pk.eyJ1IjoiaWxvdmVub29kbGVzIiwiYSI6ImNrc2NtMDNhazBpNGMyd3FrcmducWkxZTIifQ.rO0c2UpYlVFjmOt8gkUdaQ"
-        ></ReactMapGL>
+        >
+          {dataSampah.listSampah.map((item, index) => (
+            <Marker
+              key={index}
+              latitude={item.location.coordinates[1]}
+              longitude={item.location.coordinates[0]}
+            >
+              <img
+                src={pinpointImg}
+                alt=""
+                onClick={(e) => {
+                  e.preventDefault();
+                  setSelectedLocation(item);
+                  console.log("GARE");
+                }}
+              />
+            </Marker>
+          ))}
+          {selectedLocation ? (
+            <Popup
+              latitude={selectedLocation.location.coordinates[1]}
+              longitude={selectedLocation.location.coordinates[0]}
+              onClose={() =>{
+                setSelectedLocation(null)
+              }}
+            >
+              <div className='popup-maps'>
+                <h2>{selectedLocation.title}</h2>
+                <p className='center-text-desc'>{selectedLocation.desc}</p>
+                <p className='bottom-text-desc'>reported by: {selectedLocation.username}</p>
+              </div>
+            </Popup>
+          ) : null}
+        </ReactMapGL>
       </div>
     </Layout>
   );
